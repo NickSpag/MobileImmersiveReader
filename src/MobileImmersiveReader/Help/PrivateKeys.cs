7f119e73f4019741f3b0f887860edd499515f583
Nick Spagnola <spagnola8@gmail.com>
2019-07-03 10:03:19 -0700

    provide sample endpoints and an #error warning for PrivateKeys file

--- src/MobileImmersiveReader/Help/PrivateKeys.cs
+++ src/MobileImmersiveReader/Help/PrivateKeys.cs
@@ -4,12 +4,16 @@ namespace MobileImmersiveReader
 {
     public static partial class Help
     {
+
+#error Your API Keys are missing. Follow the guide in the ReadMe.md to create your Azure resources, replace the keys and endpoints here, and comment out this error to continue.
         public static class PrivateKeys
         {
             public const string ImmersiveReaderKey = "";
+            //example for West US Immersive Reader Azure Resource, found in the Overview tab of the resource: https://westus.api.cognitive.microsoft.com/sts/v1.0/issuetoken 
             public const string ImmersiveReaderEndpoint = "";
 
             public const string CognitiveServicesKey = "";
+            //example for West US Cognitive Services Azure Resource, found in the Overview tab of the resource: https://westus2.api.cognitive.microsoft.com/
             public const string CognitiveServicesEndpoint = "";
         }
     }
